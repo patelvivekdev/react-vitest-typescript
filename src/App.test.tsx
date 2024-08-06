350470cfd91d822596dcef8836116df12877c74a
VIVEK PATEL <patelvk2000@gmail.com>
2024-08-06 00:03:13 -0400

    Added prettier

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,25 +1,25 @@
-import { screen, render } from "@testing-library/react";
-import App from "./App";
-import userEvent from "@testing-library/user-event";
+import { screen, render } from '@testing-library/react';
+import App from './App';
+import userEvent from '@testing-library/user-event';
 
-describe("App tests", () => {
-  it("should render the title", () => {
+describe('App tests', () => {
+  it('should render the title', () => {
     render(<App />);
-    expect(screen.getByText("Vite + React")).toBeInTheDocument();
+    expect(screen.getByText('Vite + React')).toBeInTheDocument();
   });
 
-  it("should render the counter", () => {
+  it('should render the counter', () => {
     render(<App />);
-    expect(screen.getByText("count is 0")).toBeInTheDocument();
+    expect(screen.getByText('count is 0')).toBeInTheDocument();
   });
 
-  it("should increment the count on button click", async () => {
+  it('should increment the count on button click', async () => {
     render(<App />);
-    const button = screen.getByRole("button");
+    const button = screen.getByRole('button');
 
     const user = userEvent.setup();
     await user.click(button);
 
-    expect(screen.getByText("count is 1")).toBeInTheDocument();
+    expect(screen.getByText('count is 1')).toBeInTheDocument();
   });
 });
