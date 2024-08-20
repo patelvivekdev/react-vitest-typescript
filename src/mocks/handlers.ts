6904b3bc47063943a14c5670e4c2c7dd0fec7780
VIVEK PATEL <patelvk2000@gmail.com>
2024-08-20 14:11:08 -0400

    use msw with playwright

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { http, HttpResponse } from 'msw';
 
 export const handlers = [
-  http.get('https://jsonplaceholder.typicode.com/users/1', () => {
+  http.get('/users/1', () => {
     return HttpResponse.json({
       id: '1',
       name: 'Leanne Graham',
