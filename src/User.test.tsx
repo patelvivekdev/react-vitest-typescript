4da7c50a8303677ca2f80f9dd84adc30b5d1c99b
VIVEK PATEL <patelvk2000@gmail.com>
2024-08-20 12:50:37 -0400

    Added MSWJs

---
+++ src/User.test.tsx
@@ -0,0 +1,11 @@
+it('fetches the user info', async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+
+  expect(response.status).toBe(200);
+  expect(response.statusText).toBe('OK');
+
+  const user = await response.json();
+
+  expect(user.id).toEqual('1');
+  expect(user.name).toEqual('Leanne Graham');
+});
